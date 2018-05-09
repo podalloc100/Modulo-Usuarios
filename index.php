c91cdbd92424b1c221da2e2b15e0360b0dbc2b95
ARM <ARM@192.168.100.12>
2018-05-09 09:59:31 -0300

    Probando!

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/index.css" rel="stylesheet" type="text/css"/>
+<script src="js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#" onclick="irindex()">Inicio</a>
+  <a href="#" onclick="iralta()">Alta</a>
+  <a href="#" onclick="irbaja()">Baja y Modificación</a>    
+</div>
+    
+    
+<div id="main">
+  <h2>ABM de Usuarios</h2>
+  <p>Altas Bajas y Modificaciones de Usuario.</p>
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+</div>
+    
+
+
+</body>
+
+    <!--The Main Viewport --------------------------------------------------------------------------------------------------------------------
+    <!--
+    <tr>
+        <td id= "Vpcell" style="width:100%; position:relative; padding:0;">
+
+            <div id="MainViewPort" style="position:absolute; top:0; bottom:0; left:0px; right:0; overflow:auto; display:none;">
+               <?php echo $pagemaincontent; ?>
+            </div>
+
+        </td>
+    </tr>
+    -->
+</html> 
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
